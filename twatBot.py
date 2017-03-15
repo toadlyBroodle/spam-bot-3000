0713d73815a30438970ed302ade9ab5815ecad13
toadlyBroodle <toadlybroodle@hotmail.com>
2017-03-15 01:09:31 -0600

    combined favoriting and promoting into same function to avoid double replies to same tweet; add twitter authentication function, only called when needed; new workflows: 1) scrape and promote to all matching queries [-sp]; 2) scrape first [-s], manually edit scrapeDump.txt for relevency, then promote [-p] to remaining lines in scrapeDump.txt;

--- twatBot.py
+++ twatBot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
 import sys
 import argparse
@@ -9,26 +9,15 @@ from random import randint
 # Import our Twitter credentials from credentials.py
 from credentials import *
 
-# get authorization credentials from credentials.py
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-
-#  load promo lines (tweets) from text file
-promoFile = open('promoTweets.txt', 'r')
-p_lines = promoFile.readlines()
-promoFile.close()
-
-# load queries from text file
-queryFile = open('queries.txt', 'r')
-q_lines = queryFile.readlines()
-queryFile.close()
-
-# get number of promotional tweets and queries
-p_length = sum(1 for _ in p_lines)
-q_length = sum(1 for _ in q_lines)
+# global variables
+p_lines = []
+p_length = 0
+q_lines = []
+q_length = 0
+api = None
 
 
+# record certain events in log.txt
 def log(s):
     t = strftime("%Y%b%d %H:%M:%S", localtime()) + " " + s
 
@@ -38,6 +27,42 @@ def log(s):
     # also print truncated log to screen
     p = t[:75] + (t[75:] and '..')
     print(p)
+
+
+# get authentication credentials and tweepy api object
+def authTwitter():
+
+    global p_lines
+    global p_length
+    global q_lines
+    global q_length
+    global api
+
+    # get authorization credentials from credentials.py
+    
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    
+    # ensure authentication was successful
+    try:
+        print("Authenticating...")
+        print("Authenticated as: " + api.me().screen_name)
+    except tweepy.TweepError as e:
+        log("Failed Twitter auth: " + e.reason)
+        sys.exit(1)
+
+    #  load promo lines (tweets) from text file
+    with open('promoTweets.txt', 'r') as promoFile:
+        p_lines = promoFile.readlines()
+
+    # load queries from text file
+    with open('queries.txt', 'r') as queryFile:
+        q_lines = queryFile.readlines()
+
+    # get number of promotional tweets and queries
+    p_length = sum(1 for _ in p_lines)
+    q_length = sum(1 for _ in q_lines)
 
 
 def getRandPromo():
@@ -56,106 +81,110 @@ def updateStatus():
     except tweepy.TweepError as e:
         log(e.reason)
 
-def processTweet(tweet, fav, fol, spam):
 
-    op = tweet.user.screen_name
+def folTweeter(scrn_name):
+    try:
+        # follow, if not already
+        friends = api.get_user(scrn_name).following
+        if not friends:
+            api.create_friendship(scrn_name)
+        else:
+            log("Already following: " + scrn_name)
+            return
+    except tweepy.TweepError as e:
+        log("Error: " + e.reason)
+        return
+
+    log("Followed: " + scrn_name)
+
+
+def spamOP(twt_id, scrn_name):
+    try:
+        # reply to op with random spam
+        spamPost = getRandPromo()
+        api.update_status('@' + scrn_name + ' ' + spamPost, twt_id)
+    except tweepy.TweepError as e:
+        log("Error: " + e.reason)
+        
+    log("Spammed: " + scrn_name)
+    # seconds to wait between spam tweets
+    sleep(5)
+
+
+def replyToTweet(twt_id, scrn_name):
+    try:
+        # favorite tweet, will throw error if tweet already favorited so as to avoid double spamming
+        api.create_favorite(twt_id)
+
+        # spam op with random promo line
+        if spam:
+            spamOP(tweet.id, scrn_name)
+
+    except tweepy.TweepError as e:
+        log("Error: " + e.reason)
+        return
+
+    log("Favorited: " + scrn_name + " [" + twt_id + "]")
+
+
+def processTweet(tweet, pro, fol):
+
+    scrn_name = tweet.user.screen_name
     
     # open file in append mode
     with open('scrapeDump.txt', 'a') as f:
     
-        # append poster, time, and tweet to file
-        postTime = tweet.created_at.strftime("%Y%b%d %H:%M:%S")
-        f.write(postTime +  " @" + op + " ")
-        f.write(tweet.text + "\n")
+        # append time, tweetID, screenName, and tweetText to file
+        f.write(tweet.created_at.strftime("%b%d %H:%M"))
+        f.write("TWT_ID" + str(tweet.id))
+        f.write("SCRN_NAME" + scrn_name)
+        f.write("TWT_TXT" + tweet.text.replace("\n", "") + "\n")
     
-    # try to favorite and follow poster if not already
-    try:
-        # if already spammed post, it'll already be favorited and will throw error so won't double spam
-        if fav:
-            tweet.favorite()
-            log("Favorited @" + op + "'s post")
-        if fol and not tweet.user.following:
-            tweet.user.follow()
-            log("Followed: @" + op)
-        
-        # spam original poster with random promo line
-        if spam:
-            spamPost = getRandPromo()
-            api.update_status('@' + op + ' ' + spamPost, tweet.id)
-            log("Spammed: " + '@' + op)
-            # seconds to wait between spam tweets
-            sleep(5)
-    
-    except tweepy.TweepError as e:
-        log("Error: " + e.reason)
+    # follow op, if not already following
+    if fol and not tweet.user.following:
+        folTweeter(scrn_name)
+
+    # favorite and respond to op's tweet
+    if pro:
+        replyToTweet(tweet.id, scrn_name)
 
 
-# write file with last x many tweets of interest
-def scrapeTwitter(numItems, fav, fol, spam):
-    i = 0
+# continuously scrape for tweets matching all queries
+def scrapeTwitter(fol, pro):
    
-    # continuously execute mode
-    if numItems is 0:
-        
-        # TODO get tweets for each query
-        #tweets = []
-        #for i in range(q_length):
-             #tweets += tweepy.Cursor(api.search, q=q_lines[i]).items()
-        
-        # for now just iterate over first query
-        try:
-            q = tweepy.Cursor(api.search, q=q_lines[0]).items()
-        except tweepy.TweepError as e:
-            log("TweepyError: " + e.reason)
-        
+    for query in q_lines:
+            
+        c = tweepy.Cursor(api.search, q=query).items()
+        i = 0
         while True:
-            i += 1
             try:
-                #for q in tweets:
-                tweet = q.next()
-                processTweet(tweet, fav, fol, spam)                    
-                
+                # process next tweet
+                processTweet(c.next(), pro, fol)                    
+                i += 1 # keep track of number of tweets processed
             except tweepy.TweepError as e:
                 log(i + " tweets scraped")
-                log("TweepyError: " + e.reason)
                 
                 # wait for next request window to continue
                 time.sleep(60 * 15)
                 continue
             except StopIteration:
+                log("Scraped: " + str(i) + " tweets with: " + query.replace("\n", ""))
                 break
                 
-        log("Scraped: " + str(i) + " tweets with: " + q_lines[0])
-                
-        
-    else: # only iterate over number of tweet specified
-    
-        for query in q_lines:
-            
-            for tweet in tweepy.Cursor(api.search, q=query).items(numItems):
-                i += 1
-                #f.write(json.dumps(tweet._json) + "\n")
-                
-                processTweet(tweet, fav, fol, spam)
-                
-            log("Scraped: " + str(i) + " tweets with: " + query)
-        
     
 # get command line arguments and execute appropriate functions
 def main(argv):
 
-    parser = argparse.ArgumentParser(description="Social media promotion bot - Let's get spammy!")
+    parser = argparse.ArgumentParser(description="Beep, boop.. I'm a social media promotion bot - Let's get spammy!")
 
     subparsers = parser.add_subparsers(help='platforms', dest='platform')
     
     # Twitter
     twit_parser = subparsers.add_parser('twitter', help='Scrape twitter for all queries in queries.txt')
     group = twit_parser.add_argument_group('promotion')
-    group.add_argument('N', action='store', type=int, help='Num tweets to scrape: N<=200; N=0 when -u | -c')
-    group.add_argument('-c', '--continuous', action='store_true', dest='t_con', help='continue executing (within rate limits) until stopped')
-    group.add_argument('-a', '--favorite', action='store_true', dest='t_fav', help='favorite matching tweet')
-    group.add_argument('-o', '--follow', action='store_true', dest='t_fol', help='follow original tweeter')
-    group.add_argument('-r', '--reply', action='store_true', dest='t_rep', help='reply with random promo from promoTweets.txt ')
+    group.add_argument('-s', '--scrape', action='store_true', dest='t_scr', help='scrape for tweets matching query')
+    group.add_argument('-f', '--follow', action='store_true', dest='t_fol', help='follow original tweeters in scrapeDump.txt')
+    group.add_argument('-p', '--promote', action='store_true', dest='t_pro', help='favorite tweets and reply to tweeters in scrapeDump.txt with random promo from promoTweets.txt')
     twit_parser.add_argument('-u', '--update-status', action='store_true', dest='t_upd', help='update status with random promo from promoTweets.txt ')
     
     # Reddit
@@ -166,21 +195,39 @@ def main(argv):
     args = parser.parse_args()
     
     if args.platform == 'twitter':
-        
-        # scrape twitter for all queries and favorite, follow, and/or promote OPs
-        if args.N > 0:
-            scrapeTwitter(args.N, args.t_fav, args.t_fol, args.t_rep)
-            executed = 1
-        
-        # execute continuously, until stopped, while respecting the 900 requests per 15 minutes rate limit
-        if args.t_con:
-            scrapeTwitter(args.N, args.t_fav, args.t_fol, args.t_rep)
-            executed = 1
-            
+        # get authentication credentials and api
+        authTwitter()
+
         # update status
         if args.t_upd:
             updateStatus()
             executed = 1
+
+        # scrape twitter for all queries and favorite, follow, and/or promote OPs
+        if args.t_scr:
+            scrapeTwitter(args.t_fol, args.t_pro)
+            executed = 1
+        else: # otherwise promote to all entries in scrapeDump file
+            with open('scrapeDump.txt') as f:
+                t_lines = f.readlines()
+
+                for t in t_lines:
+                    # parse scrape dump file, seperate tweet ids from screen names
+                    a1 = t.split('TWT_ID')
+                    a2 = a1[1].split('SCRN_NAME')
+                    a3 = a2[1].split('TWT_TXT')
+                    twt_id = a2[0]
+                    scrn_name = a3[0]
+                    
+                    if args.t_fol:
+                        folTweeter(scrn_name)
+                    
+                    if args.t_pro:
+                        replyToTweet(twt_id, scrn_name)
+        
+            executed = 1
+
+
     if args.platform == 'reddit':
         print('yay reddit')
         executed = 1
