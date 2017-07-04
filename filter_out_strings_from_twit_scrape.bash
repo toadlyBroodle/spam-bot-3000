275294e0f5eabddc733657a8a6fc1692ac684764
toad <toadlybroodledev@gmail.com>
2017-07-04 16:26:49 -0600

    fix merge errors

--- filter_out_strings_from_twit_scrape.bash
+++ filter_out_strings_from_twit_scrape.bash
@@ -14,7 +14,6 @@ fi
 #done < "$1"
 
 cat ./$1/twit_scrape_dump.txt | 	# get scrape dump file from command's first argument
-<<<<<<< HEAD
 grep -iv chance | 			# remove all these keywords, ignoring case
 grep -iv instantwingame | 
 grep -iv snrtg |
@@ -25,21 +24,4 @@ grep -iv points |
 grep -iv @SYWSweeps |
 grep -iv mature |
 grep -iv wonder |
-=======
-grep -iv RT | 			# remove all these keywords, ignoring case
-grep -iv Via | 
-grep -iv spectacular |
-grep -iv points |
-grep -iv "50" |
-grep -iv Enter |
-grep -iv wonder |
-grep -iv mature |
-grep -iv @SYWSweeps |
-grep -iv PORTER-CABLE |
-grep -iv @Tool_Time_UK: |
-grep -iv https://t.co/hyMxryNTq2 |
-grep -iv https://t.co/rXwvhYF1j7 |
-grep -iv https://t.co/KWsYxZEnJm |
-grep -iv https://t.co/N8guIiTgLA |
->>>>>>> ff1600015703ad8624296aff304e75659227f18d
 tr -d '\200-\377' > ./$1/twit_scrp_dmp_filtd.txt # remove any non-ascii characters introduced by grep, and print to file
